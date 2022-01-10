f7c63690934f40a7da1909d5dc5c72026b2a2328
Eric Sellors <github@esellors.com>
2022-01-10 10:30:32 -0600

    final

---
+++ index.js
@@ -0,0 +1,14 @@
+// console.log('hello world2')
+
+
+const myFavFood = 'pizza';
+
+let myAge = 21;
+
+
+console.log(`My favorite food is ${myFavFood} and I am ${myAge}`)
+
+const num1 = 82;
+const num2 = 24;
+
+console.log(num1 + num2)
